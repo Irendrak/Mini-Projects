5f52fb1014b21f6a07012070784db79f9c95202a
Irendra K <84651018+Irendrak@users.noreply.github.com>
2021-07-20 20:59:44 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,89 +1,385 @@
-<?php
-session_start();
-error_reporting(0);
-include('includes/config.php');
-?>
-<!DOCTYPE HTML>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<title>TMS | Tourism Management System</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-
-<script type="applijewelleryion/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
-<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
-<link href="css/style.css" rel='stylesheet' type='text/css' />
-<link href='//fonts.googleapis.com/css?family=Open+Sans:400,700,600' rel='stylesheet' type='text/css'>
-<link href='//fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
-<link href='//fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
-<link href="css/font-awesome.css" rel="stylesheet">
-
-<script src="js/jquery-1.12.0.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-
-<link href="css/animate.css" rel="stylesheet" type="text/css" media="all">
-<script src="js/wow.min.js"></script>
-	<script>
-		 new WOW().init();
-	</script>
-
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+      <link href="css/bootstrap.css" rel="stylesheet"/>	
+    <link href="css/tab.css" rel="stylesheet"/>
+    <link href="css/modern-business.css" rel="stylesheet"/>
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+  <style>
+  .carousel-inner > .item > img,
+  .carousel-inner > .item > a > img {
+      width: 70%;
+      margin: auto;
+  }
+  </style>
 </head>
 <body>
-<?php include('includes/header.php');?>
-<div class="banner">
-	<div class="container">
-		<h1 class="wow zoomIn animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;"> TMS - Tourism Management System</h1>
-	</div>
-</div>
 
-<div class="container">
-	<div class="holiday">
-	
-	<h3>Package List</h3>
+   <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.html">Crime Analysis</a>
+            </div>
 
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+				
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"> Login <b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a style="cursor:pointer;" data-toggle="modal" data-target="#myModal1"> Admin </a>
+                            </li>
+
+                            <li>
+                                <a style="cursor:pointer;" data-toggle="modal" data-target="#myModal2">Station Admin</a>
+                            </li>
+
+                            <li>
+                                <a style="cursor:pointer;" data-toggle="modal" data-target="#myModal3">Employee</a>
+                            </li>
+							
+                            <li>
+                                <a style="cursor:pointer;" data-toggle="modal" data-target="#myModal4">Citizen</a>
+                            </li>
+							
+                        </ul>
+                    </li>				
+				
+                    <li>
+                        <a href="about.php">About</a>
+                    </li>
 					
-<?php $sql = "SELECT * from tbltourpackages order by rand() limit 4";
-$query = $dbh->prepare($sql);
-$query->execute();
-$results=$query->fetchAll(PDO::FETCH_OBJ);
-$cnt=1;
-if($query->rowCount() > 0)
-{
-foreach($results as $result)
-{	?>
-			<div class="rom-btm">
-				<div class="col-md-3 room-left wow fadeInLeft animated" data-wow-delay=".5s">
-					<img src="admin/pacakgeimages/<?php echo htmlentities($result->PackageImage);?>" class="img-responsive" alt="">
-				</div>
-				<div class="col-md-6 room-midle wow fadeInUp animated" data-wow-delay=".5s">
-					<h4>Package Name: <?php echo htmlentities($result->PackageName);?></h4>
-					<h6>Package Type : <?php echo htmlentities($result->PackageType);?></h6>
-					<p><b>Package Location :</b> <?php echo htmlentities($result->PackageLocation);?></p>
-					<p><b>Features</b> <?php echo htmlentities($result->PackageFetures);?></p>
-				</div>
-				<div class="col-md-3 room-right wow fadeInRight animated" data-wow-delay=".5s">
-					<h5> RUPEES <?php echo htmlentities($result->PackagePrice);?></h5>
-					<a href="package-details.php?pkgid=<?php echo htmlentities($result->PackageId);?>" class="view">Details</a>
-				</div>
-				<div class="clearfix"></div>
-			</div>
-
-<?php }} ?>
+                    <li>
+                        <a style="cursor:pointer;" data-toggle="modal" data-target="#myModal4">Status</a>
+                    </li>
 			
-		
-<div><a href="package-list.php" class="view">View More Packages</a></div>
+                    <li>
+                        <a href="citizen.php">Citizen Registeration</a>
+                    </li>
+					
+                </ul>
+            </div>
+        </div>
+    </nav>
+<br>
+<div class="container">
+  <br>
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+      <li data-target="#myCarousel" data-slide-to="3"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+      <div class="item active">
+        <img src="images/01.jpg" alt="Chania" width="460" height="345">
+      </div>
+    
+      <div class="item">
+        <img src="images/02.jpg" alt="Flower" width="460" height="345">
+      </div>
+
+      <div class="item">
+        <img src="images/02.jpg" alt="Flower" width="460" height="345">
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
 </div>
-			<div class="clearfix"></div>
+
+<!-- Admin Login -->
+	
+<div class="modal fade" id="myModal1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+	<div class="modal-dialog">
+		<form id="myForm1" name="myForm1" action="connect.php" method="post" >
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+					<h4 class="modal-title" id="myModalLabel">Admin Login</h4>
+				</div>
+
+				<div class="modal-body">
+					User-Id <span style="color:red">*</span>
+					<input type="text" name="uid" id="uid" autofocus class="form-control" required/>
+
+					Password <span style="color:red">*</span>
+					<input type="password" name="pwd" id="pwd" class="form-control" required/>
+					
+				</div>				
+			
+				<div class="modal-footer">
+					<input type="Submit" value="Submit" class="btn btn-primary">			
+				</div>
+			</div>
+		</form>	
 	</div>
+</div>	
+
+<!-- StationAdmin Login -->
+
+<div class="modal fade" id="myModal2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+	<div class="modal-dialog">
+		<form id="myForm1" name="myForm1" action="connect2.php" method="post" >
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+					<h4 class="modal-title" id="myModalLabel">Station Admin Login</h4>
+				</div>
+
+				<div class="modal-body">
+					Station-Id <span style="color:red">*</span>
+					<input type="text" name="sid" id="sid" autofocus class="form-control" required/>
+
+					Password <span style="color:red">*</span>
+					<input type="password" name="pwd" id="pwd" class="form-control" required/>
+				
+			
+					Station <span style="color:red">*</span>
+					<select name="sname1" class="form-control" id="sname1" required>
+						<option value=""> Select Station </option>
+							<?php
+								include_once('db.php');
+								
+								$qry="select distinct(stn_name) from station_admin";
+								$res = mysqli_query($con,$qry);
+								
+								while($result=mysqli_fetch_array($res))
+								{
+									echo"<option value='".$result[0]."'>".$result[0]."</option>";
+								}
+							?>
+						</select>
+						
+					<a style="cursor:pointer;" data-toggle="modal" data-target="#stnadmin">Forgot Password?</a>	
+						
+				</div>
+				
+				<div class="modal-footer">
+					<input type="Submit" value="Submit" class="btn btn-primary"/>			
+				</div>
+			</div>
+		</form>	
+	</div>
+</div>
+
+<!-- Employee Login -->
+
+<div class="modal fade" id="myModal3" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+	<div class="modal-dialog">
+		<form id="myForm1" name="myForm1" action="connect3.php" method="post" >
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+					<h4 class="modal-title" id="myModalLabel">Employee Login</h4>
+				</div>
+
+				<div class="modal-body">
+					User-Id <span style="color:red">*</span>
+					<input type="text" name="eid" id="eid" autofocus class="form-control" required/>
+
+					Password <span style="color:red">*</span>
+					<input type="password" name="epwd" id="epwd" class="form-control" required/>
+					
+					Station <span style="color:red">*</span>
+					<select name="sname" class="form-control" id="sname" required>
+							<option value=""> Select Station </option>
+								<?php
+									include_once('db.php');
+									
+									$qry="select distinct(stn_name) from station_admin";
+									$res = mysqli_query($con,$qry);
+									
+									while($result=mysqli_fetch_array($res))
+									{
+										echo"<option value='".$result[0]."'>".$result[0]."</option>";
+									}
+								?>
+						</select>
+						
+						<a style="cursor:pointer;" data-toggle="modal" data-target="#stnofficer">Forgot Password?</a>
+				</div>
+			
+				<div class="modal-footer">
+					<input type="Submit" value="Submit" class="btn btn-primary">			
+				</div>
+			</div>
+		</form>	
+	</div>
+</div>
 
 
+<!-- Citizen Login -->
+	
+<div class="modal fade" id="myModal4" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+	<div class="modal-dialog">
+		<form id="myForm1" name="myForm1" action="connect4.php" method="post" >
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+					<h4 class="modal-title" id="myModalLabel">Citizen Login</h4>
+				</div>
+
+				<div class="modal-body">
+					Email <span style="color:red">*</span>
+					<input type="text" name="c_mail" id="c_mail" autofocus class="form-control" required/>
+
+					Password <span style="color:red">*</span>
+					<input type="password" name="c_pwd" id="c_pwd" class="form-control" required/>
+					
+					<a style="cursor:pointer;" data-toggle="modal" data-target="#citizen">Forgot Password?</a>
+					
+				</div>
+			
+				<div class="modal-footer">
+					<input type="Submit" value="Submit" class="btn btn-primary">			
+				</div>
+			</div>
+		</form>	
+	</div>
+</div>
 
 
+<!-- Stn Admin Forgot Pwd  -->
 
-<?php include('includes/signup.php');?>			
+<div class="modal fade" id="stnadmin" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+	<div class="modal-dialog">
+		<form id="myForm1" name="myForm1" action="stnadmin_forgotpwd.php" method="post" >
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+					<h4 class="modal-title" id="myModalLabel">Enter E-Mail ID</h4>
+				</div>
 
-<?php include('includes/signin.php');?>			
+				<div class="modal-body">
+					Email <span style="color:red">*</span>
+					<input type="email" name="mail" id="mail" class="form-control" required/>
+					
+					Station <span style="color:red">*</span>
+					<select name="sname1" class="form-control" id="sname1" required>
+						<option value=""> Select Station </option>
+							<?php
+								include_once('db.php');
+								
+								$qry="select distinct(stn_name) from station_admin";
+								$res = mysqli_query($con,$qry);
+								
+								while($result=mysqli_fetch_array($res))
+								{
+									echo"<option value='".$result[0]."'>".$result[0]."</option>";
+								}
+							?>
+						</select>					
+					
+				</div>
+				
+				<div class="modal-footer">
+					<input type="Submit" value="Submit" class="btn btn-primary"/>			
+				</div>
+			</div>
+		</form>	
+	</div>
+</div>
 
-<?php include('includes/write-us.php');?>			
+
+<!-- Stn Officer Forgot Pwd  -->
+
+<div class="modal fade" id="stnofficer" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+	<div class="modal-dialog">
+		<form id="myForm1" name="myForm1" action="stnofficer_forgotpwd.php" method="post" >
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+					<h4 class="modal-title" id="myModalLabel">Enter E-Mail ID</h4>
+				</div>
+
+				<div class="modal-body">
+					Email <span style="color:red">*</span>
+					<input type="email" name="mail" id="mail" class="form-control" required/>
+					
+					Station <span style="color:red">*</span>
+					<select name="sname1" class="form-control" id="sname1" required>
+						<option value=""> Select Station </option>
+							<?php
+								include_once('db.php');
+								
+								$qry="select distinct(stn_name) from station_admin";
+								$res = mysqli_query($con,$qry);
+								
+								while($result=mysqli_fetch_array($res))
+								{
+									echo"<option value='".$result[0]."'>".$result[0]."</option>";
+								}
+							?>
+						</select>					
+					
+				</div>
+				
+				<div class="modal-footer">
+					<input type="Submit" value="Submit" class="btn btn-primary"/>			
+				</div>
+			</div>
+		</form>	
+	</div>
+</div>
+
+<!-- Citizen Forgot Pwd  -->
+
+<div class="modal fade" id="citizen" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+	<div class="modal-dialog">
+		<form id="myForm1" name="myForm1" action="citizen_forgotpwd.php" method="post" >
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+					<h4 class="modal-title" id="myModalLabel">Enter E-Mail ID</h4>
+				</div>
+
+				<div class="modal-body">
+					Email <span style="color:red">*</span>
+					<input type="email" name="mail" id="mail" class="form-control" required/>
+				</div>
+				
+				<div class="modal-footer">
+					<input type="Submit" value="Submit" class="btn btn-primary"/>			
+				</div>
+			</div>
+		</form>	
+	</div>
+</div>
+
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+	<script>
+		$('.carousel').carousel
+		(
+			{
+				interval: 5000
+			}
+		)
+	</script>
 </body>
 </html>
